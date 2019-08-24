68fb681e5738523a9755fe7f2c5301d548d73b84
bwalterscoding <bwalters@my.waketech.edu>
2019-08-24 12:02:05 +0000

    still heroku assets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Apples
   class Application < Rails::Application
-    config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
   end
 end
